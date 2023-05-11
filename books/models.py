566c2c561fcd75d7ab5b43b974cafc04177115c5
Dem0n009 <hedgeh0g007@gmail.com>
2023-05-11 15:30:24 +0500

    added permissions for users

--- books/models.py
+++ books/models.py
@@ -10,6 +10,8 @@ class Book(models.Model):
 	author = models.CharField(max_length=200)
 	price = models.DecimalField(max_digits=6, decimal_places=2)
 	cover = models.ImageField(upload_to='covers/', blank=True)
+	class Meta: # new
+		permissions = [('special_status', 'Can read all books'), ]
 	def __str__(self):
 		return self.title
 	def get_absolute_url(self): 
